1c6eee3edbb23288a21923b3eee779297592e494
kaosad <frasker@hotmail.com>
2015-06-17 17:19:59 +0800

    Added 2 more fields into the Token data structures
    This affects Token, IntegerToken, and OperatorToken

--- src/Token.h
+++ src/Token.h
@@ -1,50 +1,51 @@
 #ifndef Token_H
 #define Token_H
 
+#include <stdint.h>
 
 typedef enum {
-
 	TOKEN_INTEGER_TYPE,
 	TOKEN_OPERATOR_TYPE,
 } TokenType;
 
 typedef enum {
-
 	PREFIX, //-2
 	INFIX,	//2*2
 	POSTFIX	//x++
-	
 } Arity;
 
 typedef enum {
-
 	NONE,
 	LEFT_TO_RIGHT,
 	RIGHT_TO_LEFT,
-	
 } Associativity;
 
+typedef struct {
+	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
+} Token;
 
 typedef struct {
 	TokenType type;
-}Token;
-
-typedef struct {
-	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
 	int value;
-}IntegerToken;
-
+} IntegerToken;
 
 typedef struct {
 	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
 	char *symbol;
 	Arity arity;
 //	Associativity assoc;
 	Token *token[0];
-}OperatorToken;
+} OperatorToken;
 
 // symbol can be "++", "*", "[", ":"
 Token *createOperatorToken(char *symbol, Arity AR);
 Token *createIntegerToken(int value);
+
 #endif // Token_H
 
